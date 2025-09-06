4c08934dc80b7fb86a2018202c6a14a2307532a3
Andelson <devandelsongonzalez@gmail.com>
2025-09-06 13:02:09 -0400

    Update getToken.ts

--- src/context/getToken.ts
+++ src/context/getToken.ts
@@ -5,7 +5,7 @@ type ApiError = { message: string };
 export default async function getToken() {
     try {
         const response = await axios.get('https://apirestsale.onrender.com/GetToken', 
-        {headers: {passGet: '$2y$06$k6g.jmkN8MYo9cqfgmnSaOXhKxoB7tSe2E0/1rRxHQuTEEBLyHS3W'}});
+        {headers: {passget: '$2y$06$k6g.jmkN8MYo9cqfgmnSaOXhKxoB7tSe2E0/1rRxHQuTEEBLyHS3W'}});
         const  result = await response.data.AccessToken; 
 
         return result;
